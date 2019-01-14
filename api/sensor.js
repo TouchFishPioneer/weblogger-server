b4d8344d2ccebc849b2f4b1fe81a96fd732dbf80
wurahara <wurahara@126.com>
2019-01-14 10:23:11 +0800

    websocket test 04

--- api/sensor.js
+++ api/sensor.js
@@ -6,11 +6,15 @@ function sensorDataSocket (server) {
   webSocketServer.on('connection', socket => {
     console.log('websocket connection success!')
     socket.on('sensor', data => {
-      console.log(`sensor: ${data}`)
+      console.log(`sensor:
+        sampleid: ${data.sampleId},
+        pin: ${data.pin}`)
     })
 
     socket.on('rollback', data => {
-      console.log(`rollback: ${data}`)
+      console.log(`rollback:
+        sampleId: ${data.sampleId},
+        pin: ${data.pin}`)
     })
 
     socket.on('log-complete', data => {
