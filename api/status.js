708408649a198e2361e83e920ba6cfc294cd767f
wurahara <wurahara@126.com>
2019-01-15 19:23:40 +0800

    database aggregate 11

--- api/status.js
+++ api/status.js
@@ -4,22 +4,37 @@ const StatusModel = require('../database/models/status')
 // GET /status
 // Request the information of users and devices
 router.get('/status', async (ctx, next) => {
-  ctx.response.type = 'json'
-  StatusModel.aggregate([{
-    $group: {
-      _id: '$userAgent.osname',
-      total: {
-        $sum: 1
-      }
-    }
-  }]).then((res) => {
-    ctx.response.body = {
-      code: 200,
-      data: {
-        res
-      }
-    }
+  let osName
+  let deviceVender
+  let engineName
+
+  await StatusModel.aggregate([
+    { $group: { _id: '$userAgent.osname', total: { $sum: 1 } } }
+  ]).then((res) => {
+    osName = res
   })
+
+  await StatusModel.aggregate([
+    { $group: { _id: '$userAgent.devicevender', total: { $sum: 1 } } }
+  ]).then((res) => {
+    deviceVender = res
+  })
+
+  await StatusModel.aggregate([
+    { $group: { _id: '$userAgent.enginename', total: { $sum: 1 } } }
+  ]).then((res) => {
+    engineName = res
+  })
+
+  ctx.response.type = 'json'
+  ctx.response.body = {
+    code: 200,
+    data: {
+      os: osName,
+      vender: deviceVender,
+      engine: engineName
+    }
+  }
 })
 
 module.exports = router
