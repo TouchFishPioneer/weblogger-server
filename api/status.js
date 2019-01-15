ae3fb55b0112ca11dd75569986089d6a30e52c58
wurahara <wurahara@126.com>
2019-01-15 18:33:25 +0800

    database aggregate 03

--- api/status.js
+++ api/status.js
@@ -4,20 +4,27 @@ const StatusModel = require('../database/models/status')
 // GET /status
 // Request the information of users and devices
 router.get('/status', async (ctx, next) => {
-  const statusTest = StatusModel.aggregate([{
+  let result
+  StatusModel.aggregate([{
     $group: {
       _id: '$userAgent.osname',
       num: {
         $sum: 1
       }
     }
-  }])
+  }], (err, res) => {
+    if (err) {
+      console.log(err)
+    } else {
+      result = res
+    }
+  })
 
   ctx.response.type = 'json'
   ctx.response.body = {
     code: 200,
     data: {
-      statusTest
+      result
     }
   }
 
