96bcc531d7d343d60bde4890cb4d9f84f480d722
wurahara <wurahara@126.com>
2019-01-16 14:04:23 +0800

    finish

--- api/status.js
+++ api/status.js
@@ -9,19 +9,19 @@ router.get('/status', async (ctx, next) => {
   let engineName
 
   await StatusModel.aggregate([
-    { $group: { _id: '$userAgent.osname', name: '$userAgent.osname', value: { $sum: '$pinsCount' } } }
+    { $group: { _id: '$userAgent.osname', value: { $sum: '$pinsCount' } } }
   ]).then((res) => {
     osName = res
   })
 
   await StatusModel.aggregate([
-    { $group: { _id: '$userAgent.devicevender', name: '$userAgent.devicevender', total: { $sum: '$pinsCount' } } }
+    { $group: { _id: '$userAgent.devicevender', value: { $sum: '$pinsCount' } } }
   ]).then((res) => {
     deviceVender = res
   })
 
   await StatusModel.aggregate([
-    { $group: { _id: '$userAgent.enginename', name: '$userAgent.enginename', total: { $sum: '$pinsCount' } } }
+    { $group: { _id: '$userAgent.enginename', value: { $sum: '$pinsCount' } } }
   ]).then((res) => {
     engineName = res
   })
